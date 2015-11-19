4b24195f052fbe25abad4aab8262b06f890bd546
Ewout Prangsma <ewout@prangsma.net>
2015-11-19 14:13:30 +0100

    Added distance calculation

--- location/location.go
+++ location/location.go
@@ -1,5 +1,17 @@
 package location
 
+import (
+	"math"
+)
+
+type DistanceUnit float64
+
+const (
+	DistanceUnitMeter     = DistanceUnit(6367000.0)
+	DistanceUnitKilometer = DistanceUnit(6367.0)
+	DistanceUnitMile      = DistanceUnit(3956.0)
+)
+
 type Location struct {
 	Latitude  float64 // In degrees
 	Longitude float64 // In degrees
@@ -13,4 +25,17 @@ type LocationManager interface {
 func GetLocationManager() LocationManager {
 	lm := getLocationManager()
 	return lm
+}
+
+const (
+	d2r = math.Pi / 180.0
+)
+
+// Distance calculated the distance between l and lat,lon location in the given unit.
+func (l Location) Distance(lat, lon float64, unit DistanceUnit) float64 {
+	dlong := (lon - l.Longitude) * d2r
+	dlat := (lat - l.Latitude) * d2r
+	a := math.Pow(math.Sin(dlat/2.0), 2) + math.Cos(lat*d2r)*math.Cos(l.Latitude*d2r)*math.Pow(math.Sin(dlong/2.0), 2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	return float64(unit) * c
 }
